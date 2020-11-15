f62c07712edcf6bad365c3657e8d74bf5addba75
fottografie <65024966+fottografie@users.noreply.github.com>
2020-11-15 16:58:35 +0100

    Test28

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -31,38 +31,38 @@ function Virus(x, y, name, bev, infiziert, gestorben) {
     this.gestorben = gestorben;
   }
   
- Virus.prototype.display = () => {
-    s.translate(this.x, this.y);
+ Virus.prototype.display = (x, y, r, name, bev, gestorben, model) => {
+    s.translate(x, y);
     
     s.push();
-    s.scale(this.r * 25);
-    s.rotateX(frameCount * this.r);
-    s.rotateY(frameCount * this.r);
+    s.scale(r * 25);
+    s.rotateX(s.frameCount * r);
+    s.rotateY(s.frameCount * r);
     
-    var c = map(this.gestorben/this.bev, 0, 0.0002, 255, 40);
+    var c = s.map(gestorben/bev, 0, 0.0002, 255, 40);
     s.ambientMaterial(c, 0, 0);
     s.noStroke();
-    s.model(this.model);
+    s.model(model);
     s.pop();
     
     if(highlight){
       s.push();
-      s.textAlign(CENTER, CENTER);
+      s.textAlign(s.CENTER, s.CENTER);
       s.textFont(myFont);
       s.translate(0, 0, 10);
       s.fill(c, 0, 0);
-      s.text(this.name, 0, this.r * 3000 + 5);
+      s.text(name, 0, r * 3000 + 5);
       s.pop();
     }
-    s.translate(-this.x, -this.y);
+    s.translate(-x, -y);
   };
   
   
-  Virus.prototype.movement = () => {
+  Virus.prototype.movement = (x, y) => {
     var xspeed;
     var yspeed;
-    var tempX = this.x;
-    var tempY = this.y;
+    var tempX = x;
+    var tempY = y;
     var margin = 10;
     
     var nF = 80;
@@ -85,22 +85,6 @@ function Virus(x, y, name, bev, infiziert, gestorben) {
 
 s.setup = () => {
   s.createCanvas(600, 600, s.WEBGL);
-  
-  //var count = Object.keys(data).length;
-  //for(let i = 0; i < count; i++){
-  //  virus[i] = new Virus(random(-200, 200), random(-200, 200), data[i].Bundesland, data[i].Einwohner, data[i].Infektionen, data[i].Todesfälle);
-  //} 
-  
-  //let k = 0;
-  //for(let i = 0; i < cols; i++){
-  //  for(let j = 0; j < rows; j++){
-  //    let x = map(i, 0, 3, -200, 200);
-  //    let y = map(j, 0, 3, -200, 200);
-  //    virus[k] = new Virus(x, y, data[k].Bundesland, data[k].Einwohner, data[k].Infektionen, data[k].Todesfälle);
-  //    
-  //    k++;
-  //  }
-  //}
   
   var index = 0;
   virus[index] = new Virus(-60, 170, data[index].Bundesland, data[index].Einwohner, data[index].Infektionen, data[index].Todesfälle);
@@ -153,7 +137,6 @@ s.setup = () => {
 
 s.draw = () => {
   s.background(200);
-  //image(deutschland, -300, -300, 600, 600);
   
   let locX = s.mouseX - s.height / 2;
   let locY = s.mouseY - s.width / 2;
@@ -162,8 +145,8 @@ s.draw = () => {
   s.pointLight(255, 255, 255, locX, locY, 100);
   
   for(let j = 0; j < virus.length; j++){
-    virus[j].movement();
-    virus[j].display();
+    virus[j].movement(virus[j].x, virus[j].y);
+    virus[j].display(virus[j].x, virus[j].y, virus[j].r, virus[j].name, virus[j].bev, virus[j].gestorben, virus[j].model);
   }  
 };
 
