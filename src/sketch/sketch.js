3849b278ef95a95991ef6bb57071999f7c14df57
fottografie <65024966+fottografie@users.noreply.github.com>
2020-11-15 15:05:07 +0100

    Test13

--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -13,6 +13,7 @@ export default function sketch(s) {
     s.createCanvas(width, height, s.WEBGL);
   };
 
+    
   s.draw = () => {
     s.background(200);
     s.scale(0.4); // Scaled to make model fit into canvas
